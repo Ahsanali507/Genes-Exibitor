d4e25c0a974da448568b4913d1fe0fea1c5026fa
Ahsanali507 <ahmad507eb@gmail.com>
2024-08-15 20:02:09 +0500

    adding registration image

--- client/src/components/shared/RegistrationPart.jsx
+++ client/src/components/shared/RegistrationPart.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import registrationImage from '../../assets/registrationPart.svg';
 
 const RegistrationPart = () => {
-  return (
-    <div className="w-1/2 bg-gray-200 flex items-center justify-center">
-        <img src="path-to-your-image.jpg" alt="registration" className="object-cover h-full w-full" />
-    </div>
-  )
+    return (
+        <div className="w-1/2 bg-gray-200 flex items-center justify-center">
+            <img src={registrationImage} alt="registration" className="object-cover h-full w-full" />
+        </div>
+    )
 }
 
 export default RegistrationPart
